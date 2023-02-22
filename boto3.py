68be5b8aca8b882f7277dd5b059a9110d41ede05
Sahithibade <SXB72020@UCMO.EDU>
2023-02-21 20:19:38 -0600

    version

--- boto3.py
+++ boto3.py
@@ -1,1 +1,8 @@
-print
+import boto3
+session = boto3.Session( aws_access_key_id='<your_access_key_id>', aws_secret_access_key='<your_secret_access_key>')
+s3 = session.resource('s3')
+
+my_bucket = s3.Bucket('stackvidhya')
+
+for my_bucket_object in my_bucket.objects.all():
+    print(my_bucket_object.key)
